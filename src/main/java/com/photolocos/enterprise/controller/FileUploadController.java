7f92cd56c37072330033ecf7b4b537dc84c873e6
gramanma <55257671+gramanma@users.noreply.github.com>
2021-03-12 20:56:51 -0500

    Added drag and drop menu to upload menu.

--- src/main/java/com/photolocos/enterprise/controller/FileUploadController.java
+++ src/main/java/com/photolocos/enterprise/controller/FileUploadController.java
@@ -13,6 +13,16 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/upload")
 public class FileUploadController {
 
+    /**
+     * What do we want to do when the file is uploaded?
+     * Currently, just prints the original file name and does nothing.
+     * A PHP script and be used to upload this to a server, though.
+     * If someone has a better idea, please use it.
+     *
+     * Logic to decide what happens on file upload.
+     *
+     * @param file
+     */
     @PostMapping
     public void upload(@RequestParam("file") MultipartFile file) {
         log.info("uploaded file " + file.getOriginalFilename());
