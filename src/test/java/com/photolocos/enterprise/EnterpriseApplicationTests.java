43c209fc5a16a89f6664065d1e4606974adf160b
torserver <torcatobvaz@gmail.com>
2021-03-18 17:06:24 -0400

    fixed unitTests

--- src/test/java/com/photolocos/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/photolocos/enterprise/EnterpriseApplicationTests.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -29,36 +31,44 @@ class EnterpriseApplicationTests {
     void contextLoads() {
     }
 
-//    @Test
-//    void fetchPhotoByTag_returnsRiverForTagRiver() throws Exception {
-//        givenPhotoDataAreAvailable();
-//        whenPhotoAddedWithTagRiver();
-//        whenSearchPhotoWithTagRiver();
-//        thenReturnPhotosWithTagRiver();
-//    }
-//
-//    private void whenPhotoAddedWithTagRiver() {
-//        String[] tags = {"river"};
-//        photo.setTags(tags);
-//        Mockito.when(photoDAO.fetchByTag(tags)).thenReturn((Set<PhotoDTO>) photo);
-//    }
-//
-//    private void givenPhotoDataAreAvailable() {
-//        //Mockito.when(PhotoDAO.save(photo)).thenReturn(photo);
-//        /**
-//         * Save method not yet created in PhotoDAO
-//         * */
-//        photoDAO.save(photo);
-//    }
-//
-//    private void whenSearchPhotoWithTagRiver() {
-//        String[] tag = {"river"};
-//        photo = (PhotoDTO) photoDAO.fetchByTag(tag);
-//    }
-//
-//    private PhotoDTO thenReturnPhotosWithTagRiver() {
-//        return photo;
-//    }
-//
+    @Test
+    void fetchPhotoByTag_returnsRiverForTagRiver() throws Exception {
+        givenPhotoDataAreAvailable();
+        whenPhotoAddedWithTagRiver();
+        whenSearchPhotoWithTagRiver();
+        thenReturnPhotosWithTagRiver((Set<PhotoDTO>) photo);
+    }
 
+    private void whenPhotoAddedWithTagRiver() {
+        String[] tags = {"river"};
+        photo.setTags(tags);
+        Mockito.when(photoDAO.fetchByTag(tags)).thenReturn((Set<PhotoDTO>) photo);
+    }
+
+    private void givenPhotoDataAreAvailable() {
+        //Mockito.when(PhotoDAO.save(photo)).thenReturn(photo);
+        /**
+         * Save method not yet created in PhotoDAO
+         * */
+        photoDAO.save(photo);
+    }
+
+    private void whenSearchPhotoWithTagRiver() {
+        String[] tag = {"river"};
+        photo = (PhotoDTO) photoDAO.fetchByTag(tag);
+    }
+
+    private void thenReturnPhotosWithTagRiver(Set<PhotoDTO> results) {
+        Assert.notNull(results, "Photo search results returned null");
+        Assert.notEmpty(results, "Photo search results returned empty set.");
+
+        boolean photosMatch = true;
+        for (PhotoDTO photo : results) {
+            if (!Arrays.asList(photo.getTags()).contains("river")) {
+                photosMatch = false;
+            }
+        }
+
+        Assert.isTrue(photosMatch, "Photo search returned an incorrect result set.");
+    }
 }
