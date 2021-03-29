91001aaf762a65ed7c196c79b19fc1729eae2109
Paramjyot Sandhu <sandhups@mail.uc.edu>
2021-03-28 21:50:09 -0400

    Added Service class and Mechanism
    Added Service class and Mechanism

---
+++ src/main/java/com/photolocos/enterprise/service/IPhotoService.java
@@ -0,0 +1,22 @@
+package com.photolocos.enterprise.service;
+
+import com.photolocos.enterprise.dto.LocationDTO;
+import com.photolocos.enterprise.dto.PhotoDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Set;
+
+public interface IPhotoService {
+
+    PhotoDTO fetchByLocation(String location);
+
+    Set<PhotoDTO> fetchByTag(String tag);
+
+    PhotoDTO savePhoto(PhotoDTO photo, MultipartFile image);
+
+    Set<PhotoDTO> fetchAll();
+
+    Set<PhotoDTO> fetchPhotoByCity(String city);
+
+    Set<LocationDTO> fetchLocationByCity(String city);
+}
